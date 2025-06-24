99efc3c576e08f31b3d2164abe911d322bf2756e
Arunabho Bhattacharya <barunabho@gmail.com>
2025-06-24 21:15:49 +0530

    error handler upd

--- apps/auth-service/src/main.ts
+++ apps/auth-service/src/main.ts
@@ -1,14 +1,27 @@
 import express from 'express';
+import cors from 'cors';
 
-const host = process.env.HOST ?? 'localhost';
-const port = process.env.PORT ? Number(process.env.PORT) : 6001;
 
 const app = express();
+
+app.use(
+	cors({
+		origin: ["https://localhost:3000"],
+		allowedHeaders: ['Authorization', 'Content-Type'],
+		credentials: true,
+	}),
+);
 
 app.get('/', (req, res) => {
     res.send({ 'message': 'Hello API my friend'});
 });
 
-app.listen(port, host, () => {
-    console.log(`[ ready ] http://${host}:${port}`);
+const port = process.env.PORT || 6001;
+
+const server = app.listen(port, () => {
+    console.log(`Auth Service is running on http://localhost:${port}/api`);
 });
+
+server.on("error", (err) => {
+    console.log("server error", err);
+})
