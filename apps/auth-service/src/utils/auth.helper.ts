28ceb9549c36f91bdffc7adb02db306250bb3cee
Arunabho Bhattacharya <barunabho@gmail.com>
2025-07-02 09:14:59 +0530

    auth service otp verification upd

---
+++ apps/auth-service/src/utils/auth.helper.ts
@@ -0,0 +1,47 @@
+import crypto from 'crypto';
+import { ValidationError } from '../../../../packages/error-handler';
+import redis from '../../../../packages/libs/redis';
+import { sendEmail } from './sendMail';
+import { NextFunction } from 'express';
+
+const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+
+export const validateRegistrationData = (data: any, userType: "user" | "seller") => {
+	const { name, email, password, phone_number, country } = data;
+
+	if (!name || !email || !password || (userType === "seller" && (!phone_number || !country))) {
+		throw new ValidationError(`Missing required fields!`);
+	}
+
+	if (!emailRegex.test(email)) {
+		throw new ValidationError("Invalid email format!");
+	}
+};
+
+export const checkOtpRestrictions = async (email: string, next: NextFunction) => {
+	if (await redis.get(`otp_lock:${email}`)) {
+		return next(new ValidationError("Account locked due to multiple failed attempts! Try again after 30 minutes"));
+	}
+
+	if (await redis.get(`otp_spam_lock:${email}`)) {
+		return next(
+			new ValidationError("Too many OTP requests! Please wait 1 hour before requesting again.")
+		);
+	}
+
+	if(await redis.get(`otp_cooldown:${email}`)){
+		return next(
+			new ValidationError("Please wait 1 minute before requesting a new OTP.")
+		);
+	}
+};
+
+export const sendOtp = async (name: string, email: string, template: string) => {
+	const otp = crypto.randomInt(1000, 9999).toString(); // Generate a 4-digit OTP
+
+	await sendEmail(email, "Verify your email", template, { name, otp });
+
+	await redis.set(`otp:${email}`, otp, "EX", 300); // 5 minutes expiration
+
+	await redis.set(`otp_cooldown:${email}`, "true", "EX", 60);  // 1 minute cooldown
+};
